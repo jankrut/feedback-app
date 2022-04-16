5ae9c158a9f410481e9733786b2cf9ae62594083
Jankrut Pandit <jinkipandit@gmail.com>
2022-04-16 14:12:13 +0530

    no-used-vars removed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 
 import Header from "./components/Header";
-import FeedbackData from "./data/FeedbackData";
 import AboutPage from "./pages/AboutPage";
 import AboutIconLink from "./components/AboutIconLink";
 import { FeedbackProvider } from "./contexts/FeedbackContext";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
   return (
     <FeedbackProvider>
       <Router>
